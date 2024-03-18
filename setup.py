e4f3de3380bc95f5c7bd00a76ca1cd9d1200063e
Saket Satpute <satputesaket@gmail.com>
2024-03-18 20:51:24 +0530

    few minor changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="diabetes",
+    version="0.0.0",
+    author="Saket",
+    author_email="satputesaket@gmail.com",
+    packages=find_packages()
+)
