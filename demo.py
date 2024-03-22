b8f22ae45874f32263309010008414f36ec0c02d
satputesaket <satputesaket@gmail.com>
2024-03-22 18:10:44 +0530

    added exception

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
 from diabetes.logger import logging 
-
-logging.info("welcome to custom logss")
+from diabetes.exception import DiabetesDataException
+import sys
+try:
+    a = 14/"dfg"
+except Exception as e:
+    logging.error(e)
+    raise DiabetesDataException(e,sys) from e
